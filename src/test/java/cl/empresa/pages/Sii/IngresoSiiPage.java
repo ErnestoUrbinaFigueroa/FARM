ad66362d548d7bb0110131266f42b6456051a9c6
ErnestoUrbinaFigueroa <ernesto.urbina.f@gmail.com>
2018-12-03 12:56:55 -0300

    first commit

---
+++ src/test/java/cl/empresa/pages/Sii/IngresoSiiPage.java
@@ -0,0 +1,18 @@
+package cl.empresa.pages.Sii;
+
+import org.openqa.selenium.By;
+
+public class IngresoSiiPage {
+	private By menuInicial;
+	private By menuSecundario;
+
+	public IngresoSiiPage() {
+		this.menuInicial = null;
+		this.menuSecundario = null;
+	}
+
+	public void seleccionarMenu(String menuInicial, String menuSecundario) {
+		// TODO construir
+	}
+
+}
---
+++ src/test/java/cl/empresa/pages/falabella/InicialPage.java
@@ -0,0 +1,28 @@
+package cl.empresa.pages.falabella;
+
+import org.openqa.selenium.By;
+
+public class InicialPage {
+	private By buscador;
+	private By inicioSesion;
+	private By menuPrincipal;
+	private By subMenu;
+
+	public InicialPage() {
+		this.buscador = null;
+		this.inicioSesion = null;
+	}
+
+	public void iniciarSesion() {
+		// TODO construir
+	}
+
+	public void realizarBusqueda(String objeto) {
+		// TODO construir
+	}
+
+	public void seleccionarMenu(String menuPpal, String menuSec) {
+		// TODO construir
+	}
+
+}
